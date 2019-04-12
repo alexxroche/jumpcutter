ed8d37669f982860810405fce999d5dfd18533b9
Alexx Roche <notice-dev@alexx.net>
2019-04-12 14:19:58 +0200

    installing all of NixOS/nix seems like overkill
    do.sh should be enough

--- do.sh
+++ do.sh
@@ -12,6 +12,17 @@ fi
 # install deps
 pip install -r requirements.txt
 
+# build using nix package manager
+sudo groupadd -r nixbld && \
+for n in $(seq 1 10); do sudo useradd -c "Nix build user $n" \
+    -d /var/empty -g nixbld -G nixbld -M -N -r -s "$(which nologin||echo '/usr/sbin/nologin')" \
+    nixbld$n; done
+
+[ "$(which nix-build)" ]|| \
+bash <(curl https://nixos.org/nix/install)
+#curl https://nixos.org/nix/install| sudo su 
+nix-build
+
 # unit test? 
 
 # deactivate virtualenv
